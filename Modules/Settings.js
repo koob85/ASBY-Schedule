b533b70471fba1cf62a06badf45a0a3da3e95c41
matty b <koob85gaming@gmail.com>
2025-11-16 14:09:44 -0600

    yes

---
+++ Modules/Settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+	// Three letter airport code of your base
+	MyBase: "ABC",
+
+	// Your name in "Last, First" format
+	MyName: "Last, First",
+
+	// Your credentials for NOC
+	MyCredentials: {
+		AvxUserHash: "",
+		AWSALB: "",
+		AWSALBCORS: "",
+	},
+
+};
